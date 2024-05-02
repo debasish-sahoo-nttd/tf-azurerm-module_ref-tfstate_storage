e3093c83b27967f3f0166b74e70522cdcbde9fac
Debasish Sahoo <debasish.sahoo@nttdata.com>
2024-05-01 21:49:40 -0400

    Copied from skeleton

---
+++ tests/testimpl/test_impl.go
@@ -0,0 +1,18 @@
+package testimpl
+
+import (
+	"regexp"
+	"testing"
+
+	"github.com/gruntwork-io/terratest/modules/terraform"
+	"github.com/launchbynttdata/lcaf-component-terratest/types"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestComposableComplete(t *testing.T, ctx types.TestContext) {
+	t.Run("TestAlwaysSucceeds", func(t *testing.T) {
+		assert.Equal(t, "foo", "foo", "Should always be the same!")
+		assert.NotEqual(t, "foo", "bar", "Should never be the same!")
+	})
+
+}
